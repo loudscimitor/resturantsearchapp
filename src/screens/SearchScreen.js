9802f46c1f0e7ea638a9d1c8ca0978a94733f28c
Soham <dixitsoham2003@gmail.com>
2024-01-29 14:00:17 +0530

    image additions with work on css and 2nd screen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,24 +1,39 @@
-import React,{useState, useEffect } from 'react';//usestate is hook, provides spcl fucntionality to the
+import React,{useState} from 'react';//usestate is hook, provides spcl fucntionality to the
 //useEffect is used to render one compenent whenever screen loads 
 //useEffect(() => {}, []) pass in empty array for only once, add value to render multiple times
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
 import SearchBar from './components/SearchBar';
 import useResults from '../hooks/useResults';
+import ResultsList from './components/ResultsList';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
 
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price === price;
+        });
+    };
+
     return(
-        <View>
+        //remove view element for react native to keep everything in the border
+        //remove  scrollview if you want to have a single page app
+        //just passing an empty container pretty much does the job.
+        <>
             <SearchBar 
             term={term} 
             onTermChange={setTerm}
             onTermSubmit ={() => searchApi(term)}
             />
            {errorMessage ? <Text> {errorMessage}</Text> :null}
-            <Text>We have found {results.length} results</Text>
-        </View>
+            
+            <ScrollView>
+            <ResultsList results ={filterResultsByPrice('$')}title = "Cost Effecttive" />
+            <ResultsList results ={filterResultsByPrice('$$')}title = "Bit Pricier" />
+            <ResultsList results ={filterResultsByPrice('$$$')}title = "Big Spender" />
+            </ScrollView>
+            </>
     );
 };
 
